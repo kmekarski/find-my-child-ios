866b08de61a586ee7344f6cf25ce2a414fc49158
Klaudiusz Mękarski <klaudiusz.mekarski@comarch.com>
2024-06-12 09:13:57 +0200

    Initial Commit

---
+++ FindMyChild/FindMyChildApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindMyChildApp.swift
+//  FindMyChild
+//
+//  Created by klaudiuszmekarski on 12/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct FindMyChildApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
